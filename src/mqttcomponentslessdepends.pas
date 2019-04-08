9e35ff1f19f07326d4665009188ae6103b113c49
Andreas Frieß <a.friess@noll-it.at>
2019-04-08 11:09:05 +0200

    Minimal version

---
+++ src/mqttcomponentslessdepends.pas
@@ -0,0 +1,24 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit mqttcomponentslessdepends;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  mqttserver, mqttsubscriptions, mqtttokenizer, mqttmessages, MQTTClient, 
+  mqttpacketdefs, mqttpackets, mqttconsts, mqttregister, Buffers, Logging, 
+  streamutils, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('mqttregister', @mqttregister.Register);
+end;
+
+initialization
+  RegisterPackage('mqttcomponentslessdepends', @Register);
+end.
